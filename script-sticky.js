41fc60bf73530057709820d6cb5bc8912972d8c4
Peeter Marvet <pets@tehnokratt.net>
2021-03-20 14:37:50 +0200

    burgeri menüü taust ja jooned

--- script-sticky.js
+++ script-sticky.js
@@ -9,8 +9,19 @@ const onScroll = () => {
     if (scroll > 10) {
         navbar.classList.add("stick");
     } else {
-        navbar.classList.remove("stick")
+        navbar.classList.remove("stick");
     }
 }
 
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll);
+
+
+var myMenu = document.getElementById('navbarNav')
+
+myMenu.addEventListener('show.bs.collapse', function (event) {
+    navbar.classList.add("burger");
+});
+
+myMenu.addEventListener('hidden.bs.collapse', function (event) {
+    navbar.classList.remove("burger");
+});
